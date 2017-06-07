a5c3e8a5a8d0d7c79a7600d4ed6d3bb8af27e84a
mhuisking <mhuisking@gmail.com>
2017-06-07 13:52:38 -0700

    WIP

--- ZipCodeIntervalMerge/src/test/java/org/huisking/simple/SimpleZipCodeMergeTest.java
+++ ZipCodeIntervalMerge/src/test/java/org/huisking/simple/SimpleZipCodeMergeTest.java
@@ -2,30 +2,29 @@ package org.huisking.simple;
 
 import org.huisking.ZipCodeInterval;
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by chuisking on 6/7/2017.
- */
 public class SimpleZipCodeMergeTest {
     @Test
     public void mergeZipList_whenReferenceDataIsMerged_returnsCorrectResult() throws Exception {
 
-        ArrayList<ZipCodeInterval> referenceArray  = Arrays.asList( { new ZipCodeInterval(94133,94133),
+        List<ZipCodeInterval> referenceArray  = Arrays.asList( new ZipCodeInterval[]{ new ZipCodeInterval(94133,94133),
                 new ZipCodeInterval(94200,94299),
                 new ZipCodeInterval(94226,94399)});
 
-        ArrayList<ZipCodeInterval> expectedResult = Arrays.asList({new ZipCodeInterval(94133,94133),
+        List<ZipCodeInterval> expectedResult = Arrays.asList(new ZipCodeInterval[]{new ZipCodeInterval(94133,94133),
                new ZipCodeInterval(94200,94399)});
 
         SimpleZipCodeMerge simpleZipCodeMerge = new SimpleZipCodeMerge(referenceArray);
         ArrayList<ZipCodeInterval> result = simpleZipCodeMerge.mergeZipList();
 
-       // AssertTrue(Arrays.deepEquals(referenceArray,expectedResult));
+        assertTrue(expectedResult.equals(result));
     }
 
 }
